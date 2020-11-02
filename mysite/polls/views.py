dbb201ab4a234a76ff07aabcf4d8e714065ae3c3
CritRate <Miro.Hud@gmail.com>
2020-11-02 20:18:24 +0100

    Writing your first Django app, part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.db.models.query_utils import Q
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. 0ed8861d is the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+
+def results(request, question_id):
+    return HttpResponse(f'You\'re looking at the results of question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
+
+
+def owner(request):
+    return HttpResponse('Hello, world. 4a29fe97 is the polls index.')
