161b1eca7e862e8f00efb244aea879472c76aeff
CritRate <Miro.Hud@gmail.com>
2020-11-10 21:47:30 +0100

    django tutorial 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,42 @@
-from django.db.models.query_utils import Q
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    return HttpResponse(f'You\'re looking at the results of question {question_id}')
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 
 def vote(request, question_id):
-    return HttpResponse(f'You\'re voting on question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didn\'t select a choice.'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 def owner(request):
-    return HttpResponse('Hello, world. 4a29fe97 is the polls index.')
+        return HttpResponse("Hello, world. 4a29fe97 is the polls owner.")
